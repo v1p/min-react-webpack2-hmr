69c06dabdbbacab69e87f2a8ff9b581381264c68
Vipul <vbhopal@gmail.com>
2017-05-25 03:32:08 +0530

    fixed content root

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
 
 const render = (Component) => {
-    const contentRoot = document.getElementById('yabolt-root');
+    const contentRoot = document.getElementById('root');
     ReactDOM.render(
         <AppContainer>
             <Component/>
